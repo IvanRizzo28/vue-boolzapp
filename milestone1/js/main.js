ebf511af7b37f841a9ee03c45d6e4c13e9d79651
IvanRizzo28 <22ivanrizzo@gmail.com>
2023-03-16 19:09:11 +0100

    fine milestone1

---
+++ milestone1/js/main.js
@@ -0,0 +1,58 @@
+const {createApp}=Vue;
+const arr=[];
+arr.length
+createApp({
+    data() {
+      return {
+        contatti:[ 
+            {
+                nome: 'Michele',
+                cognome: '',
+                img: `img/avatar_1.jpg`
+            },
+            {
+                nome: 'Giovanni',
+                cognome: '',
+                img: `img/avatar_2.jpg`
+            }
+        ],
+        conversazioni: [
+            {
+                idContatto: 0,
+                messaggi:[
+                    {
+                        testo: 'Ok',
+                        orario: '12:00',
+                        sent: true
+                    },
+                    {
+                        testo: 'Ciao',
+                        orario: '18:50',
+                        sent: false
+                    }
+                ]
+            },
+            {
+                idContatto: 1,
+                messaggi:[
+                    {
+                        testo: 'Ok',
+                        orario: '12:00',
+                        sent: true
+                    },
+                    {
+                        testo: 'Ciao',
+                        orario: '14:50',
+                        sent: false
+                    }
+                ]
+            }
+        ]
+      }
+    },
+    methods:{
+        getOrarioUltimoMessaggio(i){
+            return this.conversazioni[i].messaggi[this.conversazioni[i].messaggi.length-1].orario;
+        }
+    }
+  }).mount('#app')
